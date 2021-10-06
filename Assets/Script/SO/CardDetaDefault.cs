cc75d7c8f4c16427d90fe956c0d9a7964d480812
NagasawaYusei <curga3013@gmail.com>
2021-10-06 17:34:02 +0900

    SO途中

---
+++ Assets/Script/SO/CardDetaDefault.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create CardDeta/Default")]
+public class CardDetaDefault : ScriptableObject
+{
+	public string Name;
+	public int Volume;
+	public int Skill;
+	public int Boots;
+	public int Clank;
+}
---
+++ Assets/Script/SO/CardDetaFirst.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create CardDeta/First")]
+public class CardDetaFirst : ScriptableObject
+{
+	public string Name;
+	public int Volume;
+	public int Skill;
+	public int Boots;
+	public int Clank;
+}
