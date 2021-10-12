10107909b7dad993debbcb7192bb9b1cf9f8f59b
NagasawaYusei <curga3013@gmail.com>
2021-10-12 18:58:40 +0900

    SO完成

---
+++ Assets/Script/SO/CardDetaTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create CardDeta/Trap")]
+public class CardDetaTrap : ScriptableObject
+{
+    public string Name;
+    public int Volume;
+    public int Cost;
+    public bool Dragon;
+    public bool Fanfale;
+    public bool Dangerous;
+    public string Others;
+
+    public virtual void Text()
+    {
+        //実行内容
+    }
+}
