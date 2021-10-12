ac3b302af43791c5134b8f360732347039b3074f
NagasawaYusei <curga3013@gmail.com>
2021-10-12 12:41:38 +0900

    接地カード以外SO完成

--- Assets/Script/SO/CardDetaItem.cs
+++ Assets/Script/SO/CardDetaItem.cs
@@ -5,6 +5,19 @@ public class CardDetaItem : ScriptableObject
 {
 	public string Name;
 	public int Volume;
+	public int Cost;
+	public bool Buddy;
+	public bool Dragon;
+	public bool Jewelry;
+	public int Skill;
+	public int Boots;
+	public int Sword;
+	public int Clank;
+	public int Point;
+	public int Life;
+	public int GetSword;
+	public int GetBoots;
+	public string Others;
 
 	public virtual void Text()
 	{
